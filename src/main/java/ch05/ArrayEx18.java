6fba4fae799c4bc40cb22b8d386fe75409477097
chaechae <songchaechaes@gmail.com>
2022-10-24 16:17:00 +0900

    add Chapter5 practice

---
+++ src/main/java/ch05/ArrayEx18.java
@@ -0,0 +1,26 @@
+package ch05;
+
+public class ArrayEx18 {
+    public static void main(String[] args) {
+        int[][] score = {
+                {100, 100, 100},
+                {20, 20, 20},
+                {30, 30, 30},
+                {40, 40, 40}
+        };
+        int sum = 0;
+
+        for (int i = 0; i < score.length; i++) {
+            for (int j = 0; j < score[i].length; j++) {
+                System.out.printf("score[%d][%d] = %d\n", i, j, score[i][j]);
+            }
+        }
+
+        for (int[] temp : score) { // score의 각 요소(1차원 배열 주소)를 temp에 저장
+            for (int i : temp) {
+                sum += i;
+            }
+        }
+        System.out.println("sum = " + sum);
+    }
+}
---
+++ src/main/java/ch05/MultiArrEx2.java
@@ -0,0 +1,53 @@
+package ch05;
+
+import java.util.Scanner;
+
+public class MultiArrEx2 {
+    public static void main(String[] args) {
+        final int SIZE = 5;
+        int x, y, num;
+
+        int[][] bingo = new int[SIZE][SIZE];
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                // 1 ~ SIZE * SIZE 까지 배열 채움
+                bingo[i][j] = i * SIZE + j + 1;
+            }
+        }
+
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                x = (int) (Math.random() * SIZE);
+                y = (int) (Math.random() * SIZE);
+
+                int temp = bingo[i][j];
+                bingo[i][j] = bingo[x][y];
+                bingo[x][y] = temp;
+            }
+        }
+
+        do {
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    System.out.printf("%2d", bingo[i][j]);
+                }
+                System.out.println();
+            }
+            System.out.printf("1 ~ %d까지의 숫자를 입력하세요. (종료:0) >", SIZE * SIZE);
+            String temp = scanner.nextLine();
+            num = Integer.parseInt(temp);
+
+            outer:
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    if (bingo[i][j] == num) {
+                        bingo[i][j] = 0;
+                        break outer;
+                    }
+                }
+            }
+        } while (num != 0);
+    }
+}
