dfc1ee9e4ded7447099493cf03dd49a2eb164d8c
chaechae <songchaechaes@gmail.com>
2022-11-03 15:55:51 +0900

    add ch13 example

---
+++ src/main/java/ch13/example/ThreadEx1.java
@@ -0,0 +1,31 @@
+package ch13.example;
+
+public class ThreadEx1 {
+    public static void main(String[] args) {
+        ThreadEx1_1 t1 = new ThreadEx1_1();
+
+        Runnable r = new ThreadEx1_2();
+        Thread t2 = new Thread(r); // 생성자 Thread(Runnable target)
+
+        t1.start();
+        t2.start();
+    }
+}
+
+class ThreadEx1_1 extends Thread {
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(getName());
+        }
+    }
+}
+
+class ThreadEx1_2 implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Thread.currentThread().getName());
+        }
+    }
+}
